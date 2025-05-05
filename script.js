6fa900a0ba0debc7b61fc9d1ab9cfec46a609bd8
Ilian Metina <ilian.metina83@gmail.com>
2025-05-05 16:27:27 +0200

    new

--- script.js
+++ script.js
@@ -1,17 +1,46 @@
-function searchPokemon(){
+async function getPokemons() {
 
+    return fetch("https://pokebuildapi.fr/api/v1/pokemon/limit/100")
+    .then(response => response.json())
 
 }
 
-function clickToSearchIcon(){
+function displayPokemonLines() {
 
-    const iconSearch = document.querySelector(".search-form i");
-    iconSearch.addEventListener("click", ()=>{
+    getPokemons().then(pokemons => {
 
-        const searchBar = document.querySelector(".search-bar");
-        const searchBarInput = searchBar.textContent.toLowerCase;
-        searchPokemon();
-
+        pokemons.forEach(pokemon => {
+            console.log(pokemon.name);
+            console.log(pokemon.id);
+            const pokemonName = pokemon.name;
+            const pokemonID = pokemon.id;
+            console.log("Pokémon : " + pokemonName);
+            const template = document.querySelector(".pokemon-line-template");
+            const newPokemonLine = template.content.cloneNode(true);
+            newPokemonLine.querySelector()
+        });
     });
 
+}
+
+/* Faire une fonction pour récupérer seulement l'image pour chaque pokemon ? */
+
+function displayPokemonOnClick(event) {
+    const pokemonTagClicked = event.target;
+
+    pokemonTagClicked.dataset.id;
+
+
+    const pokemonLines = document.querySelectorAll(".pokemon-line");
+    pokemonLines.forEach(pokemon => {
+
+        pokemon.addEventListener("click", () => {
+
+            // Display le pokemon dans la colonne de droite grâce au template 
+            // "pokemon-card-template", puis display avec append
+            // la ligne en bas à droite pour l'évolution
+
+        });
+
+    });
 }
